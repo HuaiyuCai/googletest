312f7843deca8a367648e8e236bb7a53929525d6
HuaiyuCai <caihuaiyu123@163.com>
2019-11-24 13:55:11 +0800

    Update sample1_unittest.cc
    update

--- googletest/samples/sample1_unittest.cc
+++ googletest/samples/sample1_unittest.cc
@@ -103,7 +103,7 @@ TEST(FactorialTest, Zero) {
 
 // Tests factorial of positive numbers.
 TEST(FactorialTest, Positive) {
-  EXPECT_EQ(1, Factorial(1));
+  EXPECT_EQ(2, Factorial(1));
   EXPECT_EQ(2, Factorial(2));
   EXPECT_EQ(6, Factorial(3));
   EXPECT_EQ(40320, Factorial(8));
